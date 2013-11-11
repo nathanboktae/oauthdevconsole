2e1e57377ebeedc117caf9569a6d86561accac2a
Nathan Black <nathan@nathanblack.org>
2013-11-10 22:44:20 -0800

    jshint and jukebox attempt

---
+++ app/scripts/jukebox.js
@@ -0,0 +1,10 @@
+angular.module('jukebox', []).
+directive('jukebox', function() {
+  return {
+    restrict: 'E',
+    scope: {},
+    controller: function($scope, $element) {
+      
+    }
+  }
+})
